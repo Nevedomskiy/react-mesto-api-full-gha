5f234c100f851c879e698cb4712347559585e2a4
Vasiliy.Nevedomskiy <nevedomsky.vasily@yandex.ru>
2023-07-04 22:44:51 +0400

    fix cors

--- backend/middlewares/corsOptions.js
+++ backend/middlewares/corsOptions.js
@@ -10,13 +10,14 @@ module.exports = (req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
   const { method } = req;
 
+  res.header('Access-Control-Allow-Credentials', true);
+
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Credentials', true);
     return res.end();
   }
 
